dd5f241a9ec3578508e3475039393a2edd612560
sshine <simon@simonshine.dk>
2025-10-01 15:02:36 +0200

    afleveringsopgavens README er nu færdig

--- config.h
+++ config.h
@@ -7,18 +7,18 @@ struct setting_t {
     char *value;
 };
 
+// TODO: Ret `char *` til at være `struct setting_t *`
 struct config_t {
-    struct setting_t *lines[20];
+    // struct setting_t *lines[20];
+    char *lines[20];
     size_t count;
 };
-
-// Sådan er det rigtigt:
-// struct foo {};
-// typedef struct foo foo_t;
-// foo_t get_foos(void);
 
 struct config_t *read_config(char *filename);
 void print_config(struct config_t *config);
 void free_config(struct config_t *config);
+
+void print_setting(struct setting_t *setting);
+struct setting_t *setting_converter(char *line);
 
 #endif //FILESYS_CONFIG_H
